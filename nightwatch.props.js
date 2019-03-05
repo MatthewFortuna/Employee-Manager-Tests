e26008093039cbddbf20b6d25a870bcdfdc14b8c
MatthewFortuna <48000506+MatthewFortuna@users.noreply.github.com>
2019-03-04 22:38:55 -0700

    Changed Directory

--- nightwatch.props.js
+++ nightwatch.props.js
@@ -1,5 +1,5 @@
 module.exports = {
-    resourcePath: "C:/Users/werty/OneDrive/employeeManager/Nightwatch Resources/",
+    resourcePath: "C:/Users/werty/OneDrive/EmployeeManagerTests/employeeManager/Nightwatch Resources/",
     seleniumServer: "selenium-server-standalone-3.141.59.jar",
     chromedriver: "chromedriver.exe",
     geckodriver: "geckodriver.exe",
